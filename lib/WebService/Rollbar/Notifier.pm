8b521644ae46488b260ed286e3887826bee778e2
Zoffix Znet <cpan@zoffix.com>
2015-03-23 13:00:14 -0400

    First commit

---
+++ lib/WebService/Rollbar/Notifier.pm
@@ -0,0 +1,17 @@
+
+package WebService::Rollbar;
+
+use strict;
+use warnings;
+
+
+
+
+
+'
+"Most of you are familiar with the virtues of a programmer.
+ There are three, of course: laziness, impatience, and hubris."
+                                                -- Larry Wall
+';
+
+__END__
---
+++ t/00-load.t
@@ -0,0 +1,12 @@
+#!perl
+
+use strict;
+use warnings;
+
+use Test::More;
+TODO:{
+    local $TODO="Not implemented";ok 1, "test";
+}
+
+done_testing()
+
